a44acb0375e67dad11e41fc945462e1d09ac992a
Matt3o0 <83077061+duvbolone@users.noreply.github.com>
2025-05-01 12:49:05 +0200

    change URL to this repo

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-REPO="https://github.com/adsbexchange/feedclient.git"
+REPO="https://github.com/duvbolone/feedclient.git"
 BRANCH="master"
 IPATH=/usr/local/share/adsbexchange
 mkdir -p $IPATH
